e975038c7af88664a00068fdeb27d80401f2fb40
Mitali Mittal <mitalimittal@Mitalis-MacBook-Air.local>
2025-10-21 13:42:08 -0700

    added our student IDs to config file

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+import requests 
+from bs4 import BeautifulSoup
 import re
 from urllib.parse import urlparse
 
@@ -6,15 +8,10 @@ def scraper(url, resp):
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    if not is_valid(url):
+        return list()
+    attached_links = []
+    
     return list()
 
 def is_valid(url):
