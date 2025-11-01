50693a451a82d0bd79b6001fea84b5b7b12daaf4
Jasmine Marwaha <marwahaj@uci.edu>
2025-10-31 20:34:01 -0700

    added tokenizer method

--- scraper.py
+++ scraper.py
@@ -54,6 +54,43 @@ def no_follow_meta(soup):
     if robot and 'nofollow' in robot.get('content', '').lower():
         return robot and 'nofollow' in robot.get('content', '').lower()
 
+    
+def tokenizer(url):
+    #50 most common words in english
+    text_words = (soup.get_text(separator=" ")).split()
+    stop_words = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and",
+    "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", 
+    "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", 
+    "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", 
+    "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", 
+    "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", 
+    "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", 
+    "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", 
+    "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", 
+    "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", 
+    "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", 
+    "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", 
+    "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", 
+    "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", 
+    "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", 
+    "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", 
+    "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", 
+    "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", 
+    "you're", "you've", "your", "yours", "yourself", "yourselves"}
+
+    token_freq = {}
+
+    for i in text_words:
+        i = i.lower()
+        if i not in stop_words: #checks if its not one of the 50 common words
+            token_freq[i] += 1 #adds to token freq
+
+    sorted_freq = sorted(frequencies.items(), key = lambda item: item[1], reverse = True)
+    for key, val in sorted_freq[:50]:
+        print(f"{key} - {val}")
+
+
+    
 def is_valid(url):
     # Decide whether to crawl this url or not.
     # If you decide to crawl it, return True; otherwise return False.
